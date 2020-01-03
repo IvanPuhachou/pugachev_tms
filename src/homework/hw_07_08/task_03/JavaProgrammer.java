cbedaa8c78d5fe4fe7db7c471bde7beb0dafec90
Иван Пугачёв <che.pugachev@mail.ru>
2020-01-03 23:29:07 +0300

    task_03

---
+++ src/homework/hw_07_08/task_03/JavaProgrammer.java
@@ -0,0 +1,6 @@
+package homework.hw_07_08.task_03;
+
+//Есть интерфейс Programmer. Создать интерфейс JavaProgrammer, который бы наследовал интерфейс Programmer
+
+public interface JavaProgrammer extends Programmer { // отработка в интерфейсах implements и extends ?!?!
+}
---
+++ src/homework/hw_07_08/task_03/Programmer.java
@@ -0,0 +1,4 @@
+package homework.hw_07_08.task_03;
+
+public interface Programmer {
+}
