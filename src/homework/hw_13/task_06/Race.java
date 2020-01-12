1a96fe54962df0511bfacbf33976e540faf1093f
Иван Пугачёв <che.pugachev@mail.ru>
2020-01-12 22:48:50 +0300

    task_06

---
+++ src/homework/hw_13/task_06/Race.java
@@ -0,0 +1,10 @@
+package homework.hw_13.task_06;
+
+public class Race {
+
+}
+enum typeOfMedals {
+    GOLD,
+    SILVER,
+    BRONZE
+}
---
+++ src/homework/hw_13/task_06/Sportsman.java
@@ -0,0 +1,9 @@
+package homework.hw_13.task_06;
+
+import java.util.HashMap;
+
+public class Sportsman {
+    private String name;
+    private int speed;
+    private HashMap<Integer, typeOfMedals> medals;
+}
