e1774d43d5054176f12028361042ed611d569811
Иван Пугачёв <che.pugachev@mail.ru>
2020-01-24 12:51:37 +0300

    task_07

--- src/homework/hw_13/task_07/Task_07.java
+++ src/homework/hw_13/task_07/Task_07.java
@@ -3,7 +3,6 @@ package homework.hw_13.task_07;
 //При помощи лямбда выражения, написать свой компаратор, который бы сортировал лист строк в обратном порядке
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -20,7 +19,11 @@ public class Task_07 {
         names.add("Nora");
         names.add("Emily");
         names.add("Patrick");
-        Comparator<String> descendingComparator = Comparator.reverseOrder();
 
+        Comparator<String> descendingComparator = (d1, d2) -> d2.compareTo(d1);
+
+        System.out.println(names);
+        names.sort(descendingComparator);
+        System.out.println(names);
     }
 }
